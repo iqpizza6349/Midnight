9d4687d5412b025b73b3ad50256f1cf0adedcb9d
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-24 01:25:10 +0900

    :recycle: :: remove unused imports

--- Midnight-Http/src/main/java/me/iqpizza6349/midnight/presentation/ChatController.java
+++ Midnight-Http/src/main/java/me/iqpizza6349/midnight/presentation/ChatController.java
@@ -16,7 +16,6 @@ import me.iqpizza6349.midnight.model.message.Message;
 import me.iqpizza6349.midnight.util.MessageSender;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.web.bind.annotation.*;
 
